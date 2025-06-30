d871d45e3dfbc80e71e3197d3ab6da767e467e84
Kushal1907 <kusharora19072001@gmail.com>
2025-06-30 23:22:02 +0530

    Fix: Set base URL to '/' for production deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "http://localhost:5174/",
+  base: "/",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
